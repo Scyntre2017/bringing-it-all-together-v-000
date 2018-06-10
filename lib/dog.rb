135c645b4e7fea01c7bbfb92716dd470049b1baf
Christopher Jones <cjones999983@gmail.com>
2018-06-10 16:15:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -83,4 +83,14 @@ class Dog
     end
     self
   end
+
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
