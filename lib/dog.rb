4210b9c58204b50471dc34d17f9cfdb219564b64
Christopher Jones <cjones999983@gmail.com>
2018-06-10 16:11:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,7 +54,7 @@ class Dog
       dog_hash = {id: dog_data[0], name: dog_data[1], breed: dog_data[2]}
       dog = Dog.new(dog_hash)
     else
-      song = self.create(name: name, album: album)
+      dog = self.create(name: name, breed: breed)
     end
     song
   end
